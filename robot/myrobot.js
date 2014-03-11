5cdb14fa9ab99eb64420f1d1ae00b94327636a56
Melaw <99703005@nccu.edu.tw>
2014-03-11 20:46:18 +0800

    Update myrobot.js

--- robot/myrobot.js
+++ robot/myrobot.js
@@ -9,7 +9,7 @@
       this.turn_turret_right(45);
       this.turn_right(15);
       this.move_forwards(25);
-      this.shoot();
+      this.shoot(100);
     };
     prototype.onIdle = function(){
       var myAngle, forward, tinyMove, tinyShoot, leftDist, rightDist;
@@ -20,50 +20,50 @@
         tinyMove = Math.random() * 45;
         tinyShoot = Math.random() * 10;
         leftDist = myAngle + 360 - this.myVarEnemy[0].angle;
-        this.shoot();
+        this.shoot(100);
         if (leftDist > 360) {
           leftDist = leftDist - 360;
-          this.shoot();
+          this.shoot(100);
         }
         rightDist = this.myVarEnemy[0].angle - myAngle;
         if (rightDist < 0) {
           rightDist = 360 + rightDist;
-          this.shoot();
+          this.shoot(100);
         }
         if (leftDist !== rightDist) {
           if (Math.random() > 0.5) {
             forward = true;
-            this.shoot();
+            this.shoot(100);
           }
           if (leftDist > rightDist) {
             this.turn_turret_right(rightDist + 5 + tinyShoot);
-            this.shoot();
+            this.shoot(100);
           } else {
             this.turn_turret_left(leftDist + 5 + tinyShoot);
-            this.shoot();
+            this.shoot(100);
           }
           if (forward) {
             this.move_forwards(tinyMove);
-            this.shoot();
+            this.shoot(100);
           } else {
             this.move_backwards(tinyMove);
-            this.shoot();
+            this.shoot(100);
           }
-          this.shoot();
+          this.shoot(100);
         } else {
-          this.shoot();
+          this.shoot(100);
         }
         this.myVarEnemy = undefined;
       } else {
         if (this.idleCount > 3) {
           this.doSearch();
-          this.shoot();
+          this.shoot(100);
           if (this.idleCount > 4) {
             this.doSearch();
-            this.shoot();
+            this.shoot(100);
             if (this.idleCount > 5) {
               this.doSearch();
-              this.shoot();
+              this.shoot(100);
             }
           }
           return;
