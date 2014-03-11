096ae9c9870225f4fb761cdefa36753ae38afbf4
Melaw <99703005@nccu.edu.tw>
2014-03-11 18:30:01 +0800

    Update myrobot.js

--- robot/myrobot.js
+++ robot/myrobot.js
@@ -5,9 +5,9 @@
   MyRobot = (function(superclass){
     var prototype = extend$((import$(MyRobot, superclass).displayName = 'MyRobot', MyRobot), superclass).prototype, constructor = MyRobot;
     prototype.onIdle = function(){
-      this.move_forwards(0);
-      this.turn_turret_left(10);
-      this.turn_right(90);
+      this.move_forwards(50);
+      this.turn_turret_left(100);
+      this.turn_right(900);
     };
     prototype.onWallCollide = function(){
       this.move_opposide(10);
