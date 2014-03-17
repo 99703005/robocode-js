2689784604b091c3d7c27d63fc57da0aee6a4a60
Melaw <99703005@nccu.edu.tw>
2014-03-17 16:33:06 +0800

    Update myrobot.js

--- robot/myrobot.js
+++ robot/myrobot.js
@@ -18,8 +18,8 @@
       this.shoot();
       if (this.myVarEnemy) {
         forward = false;
-        tinyMove = Math.random() * 45;
-        tinyShoot = Math.random() * 10;
+        tinyMove = Math.random() * 4.5;
+        tinyShoot = Math.random() * 1;
         leftDist = myAngle + 360 - this.myVarEnemy[0].angle;
         if (leftDist > 360) {
           leftDist = leftDist - 360;
